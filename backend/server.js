4deca5f6ea6926888628796855dd242d96d57f4e
diaslilian <lilian.dias25@gmail.com>
2021-01-07 19:07:47 -0300

    create server

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import app from "./src/app";
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`App is running on the port ${port}`);
+});
---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import index from "./routes/index";
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/cria", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Connection made successfully.");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/", index);
+
+export default app;
