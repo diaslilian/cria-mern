2087599370644d5adaea88a30f85f99e31e2ba0c
diaslilian <lilian.dias25@gmail.com>
2021-01-08 15:50:51 -0300

    add authorization and token

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
+const SECRET = process.env.SECRET;
+
+export default async (request, response, next) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).send("Header authorization error.");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, SECRET);
+
+    request.email = decoded.email;
+
+    return next();
+  } catch (err) {
+    return response.status(403).send("Invalid token.");
+  }
+};
