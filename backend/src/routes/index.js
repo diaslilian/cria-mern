2c989a0df86b52a392d3d4ec14b2dd50c368eea5
diaslilian <lilian.dias25@gmail.com>
2021-01-08 15:50:07 -0300

    create routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send({
+router.get("/", (request, response) => {
+  response.status(200).send({
     title: "Test FullStack - CRIA",
     version: "1.0.0",
   });
---
+++ backend/src/routes/usersRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+import authMiddleware from "../middlewares/auth";
+
+import controller from "../controllers/usersController";
+
+router.post("/register", controller.registerUser);
+router.post("/login", controller.loginUser);
+
+router.use(authMiddleware);
+
+router.get("/info", controller.getUsers);
+
+router.put("/update/:id", controller.updateUser);
+router.put("/update-password/:id", controller.updatePassword);
+
+router.delete("/:id", controller.deleteUser);
+
+export default router;
