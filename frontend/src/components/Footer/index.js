5f9faef3a3cde1640500e1d78cce23f9fb63f8f1
diaslilian <lilian.dias25@gmail.com>
2021-01-10 23:13:01 -0300

    add footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BiHappyBeaming } from 'react-icons/bi';
+
+import { Container } from './styles';
+
+function Footer() {
+  return (
+    <Container>
+      <BiHappyBeaming size={16} />
+      <p>Lilian Dias </p>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Footer/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  color: var(--black);
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
