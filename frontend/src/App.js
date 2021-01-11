854039f436d309ebcc9622f9a1be7f48c97cdd77
diaslilian <lilian.dias25@gmail.com>
2021-01-10 23:17:04 -0300

    config app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes/index';
+import history from './services/history';
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Layout>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </Layout>
+    </Provider>
   );
 }
 
